3a244e5dce9a9fdd119d8f8bc132f9dd5537db00
Taiyaba khan <taiyabak20@gmail.com>
2024-01-08 04:21:23 +0530

    updated forgotPass api

--- backend/controllers/forgotPass.js
+++ backend/controllers/forgotPass.js
@@ -2,23 +2,30 @@ const SibApiV3Sdk = require('sib-api-v3-sdk');
 
 exports.forgotEmail =async (req, res) => {
     const client = SibApiV3Sdk.ApiClient.instance;
+    //console.log(client)
     const apiKey = client.authentications['api-key'];
-    apiKey.apiKey = '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';
+    apiKey.apiKey =`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`;
     console.log(apiKey.apiKey)
 
-    const apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
-    const emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-    emailCampaigns.name = "Campaign sent via the API";
-    emailCampaigns.subject = "My subject";
-    emailCampaigns.sender = {"name": "From name", "email":"taiyabak20@gmail.com"};
-    emailCampaigns.type = "classic";
-    emailCampaigns.htmlContent = 'Congratulations! You successfully sent this example campaign via the Brevo API.';
-    emailCampaigns.recipients = {"listIds": ['tibu0803@gmail.com']};
+    const sender = {
+        email:"taiyabak20@gmail.com"
+    }
 
-    apiInstance.createEmailCampaign(emailCampaigns).then(function(data) {
-        console.log('API called successfully. Returned data: ' + data);
-    }, function(error) {
-        console.error(error);
-    });
-};
+    const receivers = [{
+        email:"tibu0803@gmail.com"
+    }]
+
+    try {
+        const sendEmail = await apiInstance.sendTransacEmail({
+            sender,
+            to: receivers,
+            subject: 'fsdfsf',
+TextContent: 'dassfsd'
+        })
+     }
+     catch(err){
+        console.log(err)
+     }}
+    
